44a56eef3c0fa9dde81047e6bfdff189a1820e42
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2024-05-27 15:07:12 +0300

    created main layout folders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { MainHeader } from "@/components/Header/MainHeader";
+import { MainFooter } from "@/components/Footer/MainFooter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>Header</header>
+        <MainHeader />
         {children}
-        <footer>footer</footer>
+        <MainFooter />
       </body>
     </html>
   );
